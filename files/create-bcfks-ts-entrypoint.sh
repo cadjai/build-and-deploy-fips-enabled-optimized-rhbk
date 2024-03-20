f0d18d21f00482f893d1405c7b5103e9154f7ab2
Claude Adjai <cadjai@redhat.com>
2024-03-19 20:48:02 -0600

    fix empty truststore bug

--- files/create-bcfks-ts-entrypoint.sh
+++ files/create-bcfks-ts-entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-openssl pkcs12 -nokeys -in /certs/$CACHAINCERTFILE \
-	-export -out /certs/$DESTSTOREFILE.p12  -passout pass:$DESTSTOREPASS
-
-keytool -importkeystore -srckeystore /certs/$DESTSTOREFILE.p12 -srcstoretype PKCS12 \
-	-destkeystore /certs/$DESTSTOREFILE.bcfks \
-	-srcstorepass $DESTSTOREPASS \
-	-deststorepass $DESTSTOREPASS -deststoretype BCFKS \
-	-providerclass org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider \
-	-provider org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider \
-	-providerpath /opt/keycloak/providers/bc-fips-1.0.2.3.jar \
-	-J-Djava.security.properties=/usr/lib/jvm/jre-17-openjdk/conf/security/java.security \
-        -J--add-exports=java.base/sun.security.provider=ALL-UNNAMED \
-        -J--add-opens=java.base/sun.security.provider=ALL-UNNAMED \
-	-providername BCFIPS
+keytool -importcert -noprompt  -alias $CAALIAS -file /certs/$CACHAINCERTFILE \
+    -storepass $DESTSTOREPASS -keypass $DESTSTOREPASS -trustcacerts \
+    -keystore /certs/$DESTSTOREFILE.bcfks -storetype BCFKS \
+    -providerclass org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider \
+    -providerpath /opt/keycloak/providers/bc-fips-1.0.2.3.jar \
+    -providername BCFIPS -J-Djava.security.properties=/usr/lib/jvm/jre-17-openjdk/conf/security/java.security \
+    -J--add-exports=java.base/sun.security.provider=ALL-UNNAMED \
+    -J--add-opens=java.base/sun.security.provider=ALL-UNNAMED
